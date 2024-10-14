724b64757396ebdee709b026e4b7f8e3fca96912
RPDIT <rpdoherty.IT@gmail.com>
2024-10-13 20:35:01 -0400

    Continuing development on the board. Added new pieces to the document.

--- public/app.js
+++ public/app.js
@@ -1,16 +1,19 @@
 var board = document.getElementById("chessboard");
+const white_a_pawn = new Piece("p", "white", "a2", "&#x2659;");
+const white_rook = new Piece('r', "white", "a1", "&#x265C;");
+
+const default_board = [{}]
 
 
 function tester(cords) {
     console.log(`You clicked: ${cords}`);
 };
 
-function add_piece(cords) {
-    const white_a_pawn = new Piece("p","white","a2","&#x2659;");
+function add_piece(cords, piece) {
     const position = document.querySelector(`#${cords}`);
     console.log(position.innerHTML);
     if (position.innerHTML == ""){ 
-        position.innerHTML = white_a_pawn.image;
+        position.innerHTML = piece.image;
     } else {
         position.innerHTML = "";
     }
